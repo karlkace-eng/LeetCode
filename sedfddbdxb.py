21459ba36df38a05260d2cbeabf5bdcc6c8ae70d
karlkace-eng <karlkace1234@gmail.com>
2026-07-10 15:11:46 +0530

    Add sedfddbdxb

--- sedfddbdxb.py
+++ sedfddbdxb.py
@@ -13,6 +13,6 @@ class Solution:
         for i in range(1, n):
             if nums[i] - nums[i - 1] > maxDiff:
                 comp += 1
-            component[i] = comp
+            component[i] = compare
 
         return [component[u] == component[v] for u, v in queries]
